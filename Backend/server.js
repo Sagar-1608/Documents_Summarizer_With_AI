cc6951342098344d9894ebbd57810db1b53459e0
sagar-1608 <jadhavss1608@gmail.com>
2025-03-21 22:08:24 +0530

    Server initialization

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
